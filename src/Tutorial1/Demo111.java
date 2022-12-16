0d4e50707fa9c66b5d54c2a1e7ee8abb06b367c0
HP <HP@DESKTOP-LOD748U>
2022-12-17 00:31:55 +0530

    Demo111 test cases added

---
+++ src/Tutorial1/Demo111.java
@@ -0,0 +1,16 @@
+package Tutorial1;
+
+public class Demo111 {
+
+	public static void main(String[] args) {
+      String s= "java";
+      
+      
+      for(int i=s.length()-1;i>=0;i--)
+      {
+    	  
+		System.out.print(s.charAt(i));
+      }
+	}
+
+}
